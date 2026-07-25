f39176bb50a81b501ccb9456a9afc18a6aeac896
Lyon <88804554+LyonFalcao@users.noreply.github.com>
2026-07-24 21:19:45 -0300

    Remove comments from applySobelBlock function
    Removed documentation comments for applySobelBlock function.

--- src/sobel_block.h
+++ src/sobel_block.h
@@ -1,22 +1,6 @@
 #ifndef SOBEL_BLOCK_H
 #define SOBEL_BLOCK_H
 
-/**
- * Aplica Sobel em um bloco horizontal da imagem.
- *
- * input:
- *  - bloco recebido pelo processo
- *  - contém linhas de halo
- *
- * output:
- *  - somente o resultado das linhas reais
- *
- * width:
- *  - largura da imagem
- *
- * height:
- *  - altura local sem contar halos
- */
 void applySobelBlock(
     const unsigned char *input,
     unsigned char *output,
