cbfff45af06ec2f86820cb84208ee52cf54bd06d
Lyon <88804554+LyonFalcao@users.noreply.github.com>
2026-07-24 21:18:08 -0300

    Remove documentation comments from image.h
    Removed documentation comments for loadPGM, savePGM, and freeImage functions.

--- src/image.h
+++ src/image.h
@@ -9,23 +9,10 @@ typedef struct
     unsigned char *pixels;
 } Image;
 
-/**
- * Carrega uma imagem PGM (P5).
- *
- * Retorna um ponteiro para Image ou NULL em caso de erro.
- */
 Image *loadPGM(const char *filename);
 
-/**
- * Salva uma imagem PGM (P5).
- *
- * Retorna 1 em caso de sucesso ou 0 em caso de erro.
- */
 int savePGM(const char *filename, const Image *img);
 
-/**
- * Libera toda a memória da imagem.
- */
 void freeImage(Image *img);
 
 #endif
